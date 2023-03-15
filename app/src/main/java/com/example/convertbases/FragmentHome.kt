a3e925d182512faf66ce2e1064f8be7af7fe6b10
Elliot <deadman1383matinboy@gmail.com>
2023-03-15 07:27:11 +0330

    get value from edit text first and second basis and number and check is empty or not

--- app/src/main/java/com/example/convertbases/FragmentHome.kt
+++ app/src/main/java/com/example/convertbases/FragmentHome.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.convertbases.databinding.FragmentHomeBinding
 
@@ -19,5 +20,24 @@ class FragmentHome : Fragment() {
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //get value from edit text first and second basis and edit text get number
+        var firstBasis = binding.etFirstBasis.text
+        var secondBasis = binding.etSecondBasis.text
+        var number = binding.etNumber.text
+
+        //click on convert button
+        binding.btnConvert.setOnClickListener {
+            if (firstBasis.isNotEmpty() && secondBasis.isNotEmpty() && number.isNotEmpty()) {
+
+            } else {
+                Toast.makeText(context, "لطفا مقادیر خواسته شده را وارد کنید:)", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
     }
 }
